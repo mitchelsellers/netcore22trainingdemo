98f6c6d5c4e9bc1781d9ac7a9ed279c14b3e49f5
Mitchel Sellers <msellers@Iowacomputergurus.com>
2019-08-13 22:27:09 -0500

    Unit Testing Samples

---
+++ src/SampleWeb.Services.Tests/Samples/SampleDataServiceTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http.Internal;
+using SampleWeb.Services.Samples;
+using SampleWeb.Services.Samples.Models;
+using Xunit;
+
+namespace SampleWeb.Services.Tests.Samples
+{
+    public class SampleDataServiceTests
+    {
+        private readonly ISampleDataService _sampleDataService;
+
+        public SampleDataServiceTests()
+        {
+            _sampleDataService = new SampleDataService();
+        }
+
+        [Fact]
+        public void GetFormWithFileViewModel_ShouldReturnNotNullResult()
+        {
+            //Arrange
+
+            //Act
+            var result = _sampleDataService.GetFormWithFileViewModel();
+
+            //Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void GetFormWithFileViewModel_ShouldReturnFormWithFileViewModel()
+        {
+            //Arrange
+            
+            //Act
+            var result = _sampleDataService.GetFormWithFileViewModel();
+
+            //Assert
+            Assert.IsType<FormWithFileViewModel>(result);
+        }
+
+        [Fact]
+        public void ProcessFormWithFileViewModel_ShouldReturnFalse_WhenImageIsNull()
+        {
+            //Arrange
+            var model = new FormWithFileViewModel();
+
+            //Act
+            var result = _sampleDataService.ProcessFormWithFileViewModel(model);
+
+            //Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ProcessFormWithFileViewModel_ShouldReturnTrue_WhenImageIsNotNull()
+        {
+            //Arrange
+            var model = new FormWithFileViewModel{ProfilePhoto = new FormFile(null, 0, 0, "", "")};
+
+            //Act
+            var result = _sampleDataService.ProcessFormWithFileViewModel(model);
+
+            //Assert
+            Assert.True(result);
+        }
+    }
+}
---
+++ src/SampleWeb.Tests/Controllers/SampleControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using SampleWeb.Controllers;
+using SampleWeb.Services.Samples;
+using SampleWeb.Services.Samples.Models;
+using Xunit;
+
+namespace SampleWeb.Tests.Controllers
+{
+    public class SampleControllerTests
+    {
+        private readonly Mock<ISampleDataService> _sampleDataServiceMock;
+        private readonly SampleController _sampleController;
+
+        public SampleControllerTests()
+        {
+            _sampleDataServiceMock = new Mock<ISampleDataService>();
+            _sampleController = new SampleController(_sampleDataServiceMock.Object);
+        }
+
+        [Fact]
+        public void TestForm_ShouldReturnViewResult_WithDataServiceProvidedModel()
+        {
+            //Arrange
+            var expectedResult = new FormWithFileViewModel();
+            _sampleDataServiceMock.Setup(s => s.GetFormWithFileViewModel()).Returns(expectedResult).Verifiable();
+
+            //Act
+            var result = _sampleController.TestForm();
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var actualModel = Assert.IsAssignableFrom<FormWithFileViewModel>(viewResult.Model);
+            Assert.Equal(expectedResult, actualModel);
+        }
+
+        [Fact]
+        public void TestFormPost_ShouldReturnViewResult_WithModel_WhenStateErrorsExist()
+        {
+            //Arrange
+            var postedModel = new FormWithFileViewModel();
+            _sampleController.ModelState.AddModelError(nameof(postedModel.FirstName), "Oops"); //Force error
+
+            //Act
+            var result = _sampleController.TestForm(postedModel);
+
+            //Asset
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var actualModel = Assert.IsType<FormWithFileViewModel>(viewResult.Model);
+            Assert.Equal(postedModel, actualModel);
+        }
+    }
+}
